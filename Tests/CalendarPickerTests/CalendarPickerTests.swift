e415618a436655a7c4c58b2df3f3757ffa75ffe6
Nam Nguyen <itnams12@gmail.com>
2022-04-20 09:23:52 +0700

    feat: add calendar picker view

--- Tests/CalendarPickerTests/CalendarPickerTests.swift
+++ Tests/CalendarPickerTests/CalendarPickerTests.swift
@@ -6,6 +6,6 @@ final class CalendarPickerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CalendarPicker().text, "Hello, World!")
+//        XCTAssertEqual(CalendarPicker().text, "Hello, World!")
     }
 }
